e8abadf15c5cd396521d15b7070263e797d6a30b
KrishnaJaiswal-coder <jaiswalkrishnaa@gmail.com>
2024-07-17 09:06:55 +0545

    new function.py file

---
+++ function.py
@@ -0,0 +1,11 @@
+def IsGreater(a,b):
+    if(a>b):
+        print("a is greater")
+    elif(b>a):
+        print("b is greater")
+    else:
+        print("Both numbers a and b are equal")
+a=9
+b=17
+IsGreater(a,b)
+
